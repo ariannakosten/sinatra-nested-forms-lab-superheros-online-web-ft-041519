f22d6bc6b797feae8e716ede92043a82ae0182e1
Arianna Kosten <arianna.kosten@gmail.com>
2019-06-25 23:10:51 +0000

    Automatically backed up by Learn

--- spec/super_sinatra_forms_spec.rb
+++ spec/super_sinatra_forms_spec.rb
@@ -37,6 +37,7 @@ describe App do
       expect(page).to have_text("Hero Power: Ruby")
       expect(page).to have_text("Hero Biography: I love Ruby!")
       expect(page).to have_text("Hero Name: Arel")
+      binding.pry
       expect(page).to have_text("Hero Power: JavaScript")
       expect(page).to have_text("Hero Biography: I love JavaScript!")
       expect(page).to have_text("Hero Name: Katie")
